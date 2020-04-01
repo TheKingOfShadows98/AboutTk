58bed0830814aa1111ce06215bfacfdcaa29854a
Tk <thekingofshadows98@gmail.com>
2020-03-31 20:07:47 -0600

    Bug Fixing

--- decoder.js
+++ decoder.js
@@ -1,58 +1,45 @@
-
-// function readXmlFile(file = "./stories/LHDH.xml"){
-// var xdoc = new XMLDocument();
-//     xdoc.open(file);
-//     var hTitulo = "";
-//     var hParrafos = ["1"];
-
-//     var titulo = xdoc.getElementsByTagName("Titulo");
-//     var parrafos = xdoc.getElementsByTagName("parrafo");
-//     for (i = 0; i == titulo.length; i++){
-//         hParrafos.push('h2 class="titulo"'+titulo[i]+'</h2>');
-//     }
-//     for (i = 0; i == titulo.length; i++){
-//         var a = "<p class='texto'>"+obj+"</p>";
-//          hParrafos.push(a);
-//     }
-//     var result = [hTitulo, hParrafos];
-//     return result;
+// let archivo = document.getElementById("Archivo");
+// archivo.addEventListener(onchange,ini);
+// function ini(){
+//     readTextFile(archivo.value);
+//     alert("el archivo cambio");
 // }
 
-// var DOM = document;
-// var contenido = DOM.getElementById("history");
-// alert(readXmlFile().toString());
-// contenido.innerHTML = readXmlFile()[1];
-
-var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function() {
-  if (xhttp.this == 4 && this.status == 200) {
-    myFunction(this);
-  }
-};
-xhttp.open("GET", "./stories/LHDH.xml",true);
-xhttp.send();
-
-function myFunction(xml) {
-  var parser, xmlDoc;
-  parser = new DOMParser();
-  xmlDoc = parser.parseFromString(xml.responseText,"text/xml");
-  document.getElementById("demo").innerHTML =
-  myLoop(xmlDoc.documentElement);
-}
-function myLoop(x) {
-    var i, y, xLen, txt;
-    txt = "";
-    x = x.childNodes;
-    xLen = x.length;
-    for (i = 0; i < xLen ;i++) {
-      y = x[i];
-      if (y.nodeType != 3) {
-        if (y.childNodes[0] != undefined) {
-          txt += myLoop(y);
-        }
-      } else {
-      txt += y.nodeValue + "<br>";
+function loadDoc() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        organizar(this.responseText);
       }
+    };
+    xhttp.open("GET", "stories/LHDH.txt", true);
+    xhttp.send();
+}
+function organizar (texto_plano){
+    let contenidos = texto_plano.split('|');
+
+    contenidos.map(x => reemplazar(x));
+
+}
+function reemplazar(str) {
+    let res = "";
+    if(str.includes("Titulo:")){
+        document.getElementById("PageTitle").innerHTML = str.replace("Titulo:","");
+        res = str.replace("Titulo:", '<h2 class= "titulo">')
+        res += "</h2>";
+    }else if(str.includes("dialogo:")){
+        res = str.replace("dialogo:", '<p class = "dialogo-texto">');
+        res += "</p>";
     }
-    return txt;
+    if(str.includes("acotacion:")){
+      res = str.replace("acotacion:", '<p class = "acotacion-texto">');
+      res += "</p>";
   }
+  if(str.includes("narrador:")){
+    res = str.replace("narrador:", '<p class = "narrador-texto">');
+    res += "</p>";
+}
+    contenedor.innerHTML += res;
+}
+
+var contenedor = document.getElementById('demo');
